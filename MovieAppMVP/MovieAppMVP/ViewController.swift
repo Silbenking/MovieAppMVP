2210715dfdb67bdc9d71b8130001be3c4b7d5e95
silbenking <silbenking@mail.ru>
2023-12-12 23:36:09 +0300

    create project and dependencies

---
+++ MovieAppMVP/MovieAppMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppMVP
+//
+//  Created by Сергей Сырбу on 12.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
